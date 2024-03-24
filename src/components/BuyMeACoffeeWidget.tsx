0d0fdb5d8d44162a0aca5d08421361e6be5e057c
Richard <richard.m.hpa@gmail.com>
2024-03-24 15:27:14 +1300

    fixed image src

--- src/components/BuyMeACoffeeWidget.tsx
+++ src/components/BuyMeACoffeeWidget.tsx
@@ -1,7 +1,9 @@
+import image from 'images/bmc-button.png';
+
 export function BuyMeACoffeeWidget() {
   return (
     <a href="https://www.buymeacoffee.com/richardhpa" target="blank">
-      <img style={{ height: '50px' }} alt="Buy Me a Coffee Widget" src="/bmc-button.png" />
+      <img style={{ height: '50px' }} alt="Buy Me a Coffee Widget" src={image} />
     </a>
   );
 }
