30dd4162b172c3a1f72c376409c408e279be749c
Richard <richard.m.hpa@gmail.com>
2024-04-12 20:24:21 +1200

    added check to see if tournaments have started

--- src/hooks/useGetLatestTournaments/useGetLatestTournaments.tsx
+++ src/hooks/useGetLatestTournaments/useGetLatestTournaments.tsx
@@ -6,5 +6,10 @@ export const useGetLatestTournaments = (data?: Tournament[], amount = 10) => {
   if (!data) return [];
   const latestTournaments = data.filter(tournament => tournament.tournamentStatus !== RUNNING);
   // then get the latest ones
+
+  if (amount === -1) {
+    return latestTournaments;
+  }
+
   return latestTournaments.slice(0, amount);
 };
