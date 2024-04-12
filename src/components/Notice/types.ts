4e7c53fcfa380cbd5d59fe8884e7966520f94aec
Richard <richard.m.hpa@gmail.com>
2024-04-12 21:12:29 +1200

    added dismiss notice

---
+++ src/components/Notice/types.ts
@@ -0,0 +1,9 @@
+import type { ReactNode } from 'react';
+
+export interface NoticeProps {
+  status?: 'info' | 'success' | 'error' | 'warning' | 'dark';
+  children: ReactNode;
+  dismissible?: boolean;
+  noticeId?: string;
+  onDismiss?: (noticeId: string) => void;
+}
