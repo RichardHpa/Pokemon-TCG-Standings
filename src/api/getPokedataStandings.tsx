24e92c3f7c4a753b7c9be62c1dd70e155d7d9183
Richard Hpa <richard.m.hpa@gmail.com>
2024-04-15 08:25:39 +1200

    Temp use proxy server for tournaments

--- src/api/getPokedataStandings.tsx
+++ src/api/getPokedataStandings.tsx
@@ -3,12 +3,15 @@ import axios from 'axios';
 import type { Standing } from 'types/standing';
 
 export const getPokedataStandings = async (tournamentId: string) => {
-  const url = `https://www.pokedata.ovh/api/complete/?id=${tournamentId}&game=tcg&division=masters`;
+  // const url = `https://www.pokedata.ovh/api/complete/?id=${tournamentId}&game=tcg&division=masters`;
+  // const url = `https://www.pokedata.ovh/standings/${tournamentId}/masters/${tournamentId}_Masters.json`;
+  const url = `https://pokemonserver.fly.dev/standings/${tournamentId}`;
 
   const response = await axios.get(url).then(res => {
     const data = res.data;
     return data;
   });
 
-  return response.data.masters as Standing[];
+  // return response.data.masters as Standing[];
+  return response as Standing[];
 };
