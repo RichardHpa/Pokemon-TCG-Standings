f61aaef1e46f1a6e5a93b31a74fc193276e35bc6
Richard <richard.m.hpa@gmail.com>
2024-09-05 20:39:45 +1200

    added count to card

--- src/pages/Player/Decklist/Decklist.tsx
+++ src/pages/Player/Decklist/Decklist.tsx
@@ -22,6 +22,12 @@ const getImageUrl = (card: PokemonCard) => {
   return `https://images.pokemontcg.io/${setCode}/${number}.png`;
 };
 
+// TODO: move to helpers
+const getCountryCodeFromName = (name: string) => {
+  const countryCode = name.match(/\[(\w+)\]$/);
+  return countryCode ? countryCode[1] : '';
+};
+
 const useGetDecklist = (deckList: DeckList) => {
   const pokemon = deckList.pokemon;
   const formattedPokemon = pokemon.map(card => {
@@ -50,21 +56,21 @@ const useGetDecklist = (deckList: DeckList) => {
 };
 
 const DecklistInner = ({ decklist }: { decklist: DeckList }) => {
-  const { list, formattedCards } = useGetDecklist(decklist);
+  const { formattedCards } = useGetDecklist(decklist);
 
   return (
     <div>
       <div className="grid gap-2 grid-cols-8">
         {formattedCards.map(card => (
-          <div key={card.name}>
+          <div key={card.name} className="relative">
             <img src={card.image} alt={card.name} />
+
+            <div className="absolute bottom-0 right-0 inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 m-2">
+              <span className="font-medium text-gray-600 dark:text-gray-300">{card.count}</span>
+            </div>
           </div>
         ))}
       </div>
-
-      <pre>
-        <code>{JSON.stringify(list, null, 2)}</code>
-      </pre>
     </div>
   );
 };
@@ -87,5 +93,24 @@ export const Decklist = () => {
 
   const player = playerInfo[0];
 
-  return <DecklistInner decklist={player.decklist} />;
+  return (
+    <>
+      {getCountryCodeFromName(player.name) === 'JP' && (
+        <p className="italic text-gray-500 dark:text-gray-400">
+          Some cards may be wrong as we have converted the set list from the Japanese sets, if there
+          is something wrong, please raise an issue with us on{' '}
+          <a
+            href="https://github.com/RichardHpa/Pokemon-TCG-Standings/issues"
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-500 dark:text-blue-400 underline"
+          >
+            github
+          </a>{' '}
+          and we will fix it.
+        </p>
+      )}
+      <DecklistInner decklist={player.decklist} />
+    </>
+  );
 };
