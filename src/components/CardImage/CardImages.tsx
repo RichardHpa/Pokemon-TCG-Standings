5b1bba282307231826cef98b7a04ff5eea81f491
Richard <richard.m.hpa@gmail.com>
2024-09-20 21:59:57 +1200

    added failing type

--- src/components/CardImage/CardImages.tsx
+++ src/components/CardImage/CardImages.tsx
@@ -1,6 +1,12 @@
 import { useState, useCallback } from 'react';
+
 import type { FC } from 'react';
 
+const test = (a: string, b: number) => {
+    return a + b;
+};
+
+console.log(test('a', 0));
 interface CardImageProps {
     src: string;
     alt: string;
