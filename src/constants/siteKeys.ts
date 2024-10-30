57dc387b133a5714e9edb9d9be72529f5b3da90e
Richard <richard.m.hpa@gmail.com>
2024-10-31 08:41:01 +1300

    updated key

--- src/constants/siteKeys.ts
+++ src/constants/siteKeys.ts
@@ -1,4 +1,5 @@
 export const pinnedPlayersKey = 'PokemonTCGStandings:pinnedPlayersV2';
 export const tableLayoutKey = 'PokemonTCGStandings:TableLayout';
 
-export const PINNED_PLAYERS_KEY = 'PokemonTCGStandings:pinnedPlayersV3';
+export const BASE_KEY = 'PokemonTCGStandings';
+export const PINNED_PLAYERS_KEY = 'pinnedPlayersV3';
